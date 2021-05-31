8e9cc976ef91f4cd2bda8f8cbcfc118d3fea9d57
zaraconsulting <derek@zaraconsulting.org>
2021-05-31 14:21:25 -0500

    programs functionality finished

--- single-event.php
+++ single-event.php
@@ -28,6 +28,24 @@
                 <div class="generic-content">
                     <?php the_content(); ?>
                 </div>
+
+
+                    
+                <?php
+                    $relatedPrograms = get_field('related_programs'); 
+                    if ( $relatedPrograms ) { ?>
+                        <hr class="section-break" />
+                        <h2 class="headline headline--medium">Related Program(s)</h2>
+                        <ul class="link-list min-list">
+                            <?php
+                                foreach ($relatedPrograms as $program) { ?>
+                                    <li>
+                                    <a href="<?php echo get_the_permalink( $program ); ?>"><?php echo get_the_title( $program ); ?></a>
+                                    </li>
+                                <?php }
+                            ?>
+                        </ul>
+                <?php } ?>
             </div>
         <?php }
     ?>
