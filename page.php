e1359f56ad93355fe91320a673c4b39d671d7820
zaraconsulting <derek@zaraconsulting.org>
2021-05-30 19:48:50 -0500

    finished blog functionality

--- page.php
+++ page.php
@@ -6,10 +6,10 @@
         <div class="page-banner">
             <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>);"></div>
             <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title(); ?></h1>
-            <div class="page-banner__intro">
-                <p style="text-transform: uppercase;">Don't forget to replace me later</p>
-            </div>
+                <h1 class="page-banner__title"><?php the_title(); ?></h1>
+                <div class="page-banner__intro">
+                    <p style="text-transform: uppercase;">Don't forget to replace me later</p>
+                </div>
             </div>  
         </div>
 
