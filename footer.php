c90c235faa935f1a63e0f701fa1ca8a09d6684d2
zaraconsulting <derek@zaraconsulting.org>
2021-05-31 11:12:47 -0500

    events functionality working

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
                 <ul>
                   <li><a href="<?php echo site_url('about-is'); ?>">About Us</a></li>
                   <li><a href="#">Programs</a></li>
-                  <li><a href="#">Events</a></li>
+                  <li><a href="<?php echo site_url('events'); ?>">Events</a></li>
                   <li><a href="#">Campuses</a></li>
                 </ul>
                 <!-- <?php
--- functions.php
+++ functions.php
@@ -2,13 +2,24 @@
 
 function university_theme_files()
 {
-    // scripts
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
-
     // styles
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('unversity_main_styles', get_stylesheet_uri());
+    // wp_enqueue_style('unversity_main_styles', get_stylesheet_uri());
+    
+    // scripts
+
+    // wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+
+    if ( strstr($_SERVER['SERVER_NAME'], 'localhost') ) {
+        wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+    } else {
+        wp_enqueue_script('vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+        wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'));
+        wp_enqueue_style('main-styles', get_theme_file_uri('/bundled-assets/styles.bc49dbb23afb98cfc0f7.css'));
+    }
+    
+
 }
 add_action('wp_enqueue_scripts', 'university_theme_files');
 
@@ -21,3 +32,23 @@ function university_features()
     add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'university_features');
+
+function university_adjust_queries($query)
+{
+    if( !is_admin() AND is_post_type_archive('event') AND $query->is_main_query() ) 
+    {
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query', array(
+                array(
+                    'key' => 'event_date',
+                    'compare' => '>=',
+                    'value' => date( 'Ymd' ),
+                    'type' => 'numeric'
+                )
+            )
+        );
+    }
+}
+add_action('pre_get_posts', 'university_adjust_queries');
